ba434110bacd3c30f17a01ed9b95a02d3d28fa7f
Kevin Uy <kevin.uy@amkor.com>
2022-05-19 09:29:50 +0800

    HOTFIX: Resolution Center - Updated Tooltip Label

--- src/components/Dashboard/ResolutionCenter.vue
+++ src/components/Dashboard/ResolutionCenter.vue
@@ -34,7 +34,7 @@
 
                                 <q-td key="action" class="text-right" :props="props">
                                     <q-btn flat round color="standard" text-color="grey-6" icon="las la-edit" size="md" padding="xs">
-                                        <q-tooltip transition-show="scale" :delay="500" transition-hide="scale" content-class="bg-grey-12 text-black">Edit Request</q-tooltip>
+                                        <q-tooltip transition-show="scale" :delay="500" transition-hide="scale" content-class="bg-grey-12 text-black">File Request</q-tooltip>
                                     </q-btn>
                                 </q-td>
                             </q-tr>
